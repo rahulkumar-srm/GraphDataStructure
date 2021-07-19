0524f956ff68d5cec9f4efd814df8ee889de1e4f
Rahul Kumar <rahul.kumar14@stryker.com>
2021-07-19 20:44:51 +0530

    Added topological sort

---
+++ GraphDataStructure/Helper/TopologicalSort.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphDataStructure.Helper
+{
+    class TopologicalSort
+    {
+        public List<int> solve(int A, List<List<int>> B)
+        {
+            int[] indegree = new int[A + 1];
+            List<int> ans = new List<int>();
+            MinHeap minHeap = new MinHeap();
+            Dictionary<int, List<int>> graph = new Dictionary<int, List<int>>();
+
+            for (int i = 0; i < B.Count; i++)
+            {
+                if (!graph.ContainsKey(B[i][0]))
+                    graph[B[i][0]] = new List<int>();
+
+                graph[B[i][0]].Add(B[i][1]);
+                indegree[B[i][1]]++;
+            }
+
+            for (int i = 1; i <= A; i++)
+            {
+                if (indegree[i] == 0)
+                    minHeap.Insert(i);
+            }
+
+            while (minHeap.Size() > 0)
+            {
+                int node = minHeap.Delete();
+                ans.Add(node);
+
+                if (graph.ContainsKey(node))
+                {
+                    foreach (int vertex in graph[node])
+                    {
+                        if (--indegree[vertex] == 0)
+                            minHeap.Insert(vertex);
+                    }
+                }
+            }
+
+            if (ans.Count < A)
+                return new List<int>();
+
+            return ans;
+        }
+    }
+
+    class MinHeap
+    {
+        public int top;
+        public List<int> heap;
+
+        public MinHeap()
+        {
+            top = -1;
+            heap = new List<int>();
+        }
+
+        public int Size()
+        {
+            return top + 1;
+        }
+
+        public void Insert(int num)
+        {
+            int i = ++top;
+            heap.Add(0);
+
+            while (i > 0 && heap[(i - 1) / 2] > num)
+            {
+                heap[i] = heap[(i - 1) / 2];
+                i = (i - 1) / 2;
+            }
+
+            heap[i] = num;
+        }
+
+        public int Delete()
+        {
+            int res = heap[0];
+            heap[0] = heap[top];
+            heap[top--] = 0;
+
+            int i = 0, j = 2 * i + 1;
+
+            while (j <= top)
+            {
+                if (j < top && heap[j + 1] < heap[j])
+                    j += 1;
+
+                if (heap[j] < heap[i])
+                {
+                    int temp = heap[j];
+                    heap[j] = heap[i];
+                    heap[i] = temp;
+
+                    i = j;
+                    j = 2 * i + 1;
+                }
+                else
+                    break;
+            }
+
+            return res;
+        }
+    }
+}
